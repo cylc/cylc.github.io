4bb288c1d73a0c07c496baf6c16595c81fdcd642
Hilary James Oliver <hilary.j.oliver@gmail.com>
2019-02-28 09:48:16 +1300

    Updated sphinx docs.

--- doc/built-sphinx-single/_static/documentation_options.js
+++ doc/built-sphinx-single/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.8.1-6-g946ec-dirty',
+    VERSION: '7.8.1-10-g3ff61',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
